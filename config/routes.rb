31ca5a52cdb7c48a55d6a09256b49aef66c2eb46
biswasthapa <viswasthapa@gmail.com>
2017-02-09 17:52:09 +1100

    Routes to use default json format

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  post '/tag' => 'entities#create'
-  get '/tags/:entity_type/:entity_id' => 'entities#show'
-  delete '/tags/:entity_type/:entity_id'=> 'entities#destroy'
+  post '/tag' => 'entities#create', defaults: {format: :json}
+  get '/tags/:entity_type/:entity_id' => 'entities#show', defaults: {format: :json}
+  delete '/tags/:entity_type/:entity_id'=> 'entities#destroy',defaults: {format: :json}
 
-  get '/stats' => 'entities#stats'
-  get '/stats/:entity_type/:entity_id'=>'entities#get_individual_stat'
+  get '/stats' => 'entities#stats', defaults: {format: :json}
+  get '/stats/:entity_type/:entity_id'=>'entities#get_individual_stat', defaults: {format: :json}
 end
